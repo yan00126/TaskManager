f59be86ed554989692ce144facdd36d3616d2a94
FeiYan <yan00126@algonquinlive.com>
2024-08-02 02:17:06 -0400

    not complete

---
+++ Data/DataService.cs
@@ -0,0 +1,32 @@
+﻿using System.Configuration;
+using Dapper;
+using MySql.Data.MySqlClient;
+
+namespace TaskManager;
+
+public class DataService (string connectionString)
+{
+    private readonly string connectionString = connectionString;
+
+    //Load Data - retrieving data from database
+
+    public async Task<List<T>> LoadData<T, U>(string sql, U parameters)
+    {
+        using(var connection = new MySqlConnection(connectionString))
+        {
+            var rows = await connection.QueryAsync<T>(sql, parameters);
+            return rows.ToList();
+        }
+    }
+
+    //Save Data - saving or manipulating data in the database
+
+    public async Task SaveData<U>(string sql,U parameters)
+    {
+        using(var connection = new MySqlConnection(connectionString))
+        {
+            await connection.ExecuteAsync(sql, parameters);
+        }
+    }
+
+}
---
+++ Models/Item.cs
@@ -0,0 +1,12 @@
+﻿namespace TaskManager;
+
+public class Item
+{
+    public int Id {get; set;}
+    public string? Task {get;set;}
+
+    public bool IsComplete {get;set; }
+
+    public List<CheckboxItem> Categories {get;set;} = [];
+    
+}
